c27b3e4836cc2c12eb0905201f5ccb17fcae95ae
Anastasia Shchipakina <avshchipakina@yandex.ru>
2021-12-04 23:23:40 +0300

    feat: add structure

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
